f5059838ccd68862db70e2c129587d2ff288d8cd
tranhoanghuy123 <106876476+tranhoanghuy123@users.noreply.github.com>
2024-11-14 00:31:45 +0700

    Add files via upload

---
+++ DB/Scripts/Create-All-Table.sql
@@ -0,0 +1,8 @@
+create database Project_TTTN;
+use Project_TTTN
+CREATE TABLE users (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    username NVARCHAR(50) UNIQUE NOT NULL,
+    password NVARCHAR(255) NOT NULL
+);
+
